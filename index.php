2ea9774edee68b99dc40d29de911cb0ab50d618a
datdx2 <dathalongbay@gmail.com>
2019-08-27 16:56:39 +0700

    111

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     <title>Title</title>
 </head>
 <body>
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v4.0&appId=2083516388621698&autoLogAppEvents=1"></script>
     <?php echo "API"; ?>
+
+<div class="fb-like" data-href="https://facebookapidat.herokuapp.com/" data-width="100" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
 </body>
 </html>

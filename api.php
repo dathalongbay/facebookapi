c01885d17463d76a92c6d700c4e0758e03d9acf0
datdx2 <dathalongbay@gmail.com>
2019-08-27 17:01:00 +0700

    111

---
+++ api.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: datdx2
+ * Date: 8/27/2019
+ * Time: 5:00 PM
+ */
+
+function facebook_count($url){
+
+    // Query in FQL
+    $fql  = "SELECT share_count, like_count, comment_count ";
+    $fql .= " FROM link_stat WHERE url = '$url'";
+
+    $fqlURL = "https://api.facebook.com/method/fql.query?format=json&query=" . urlencode($fql);
+
+    // Facebook Response is in JSON
+    $response = file_get_contents($fqlURL);
+    return json_decode($response);
+
+}
+
+$fb = facebook_count('https://facebookapidat.herokuapp.com');
+
+// facebook share count
+echo $fb[0]->share_count;
+
+// facebook like count
+echo $fb[0]->like_count;
+
+// facebook comment count
+echo $fb[0]->comment_count;
